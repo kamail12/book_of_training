7a6a81762ac1845a3b0f182ee692cc6793ee0b51
kamail12 <kamiloxen1@gmail.com>
2023-07-29 17:25:49 +0200

    Setup File & folder structure with React-Router-DOM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+//style
+import "./App.css";
+import { Box } from "@mui/material";
+
+//Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+//Pages
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>test app</h1>
+			<Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+				<Navbar />
+
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/exercise/:id" element={<ExerciseDetail />} />
+				</Routes>
+				<Footer />
+			</Box>
 		</div>
 	);
 }
