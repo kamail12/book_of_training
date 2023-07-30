38b5e696e238c8ae805595c7638039efae81e6b5
kamail12 <kamiloxen1@gmail.com>
2023-07-30 20:05:29 +0200

    Making logic in Exercises component and create Card with exercises finally add pagination

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -3,8 +3,25 @@ import { Pagination } from "@mui/material";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { exercisesOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 const Exercises = ({ setExercise, exercises, setBodyPart }) => {
+	const [currentPage, setCurrentPage] = useState(1);
+	const exercisesPerPage = 9;
+
+	const indexOfLastExercises = currentPage * exercisesPerPage;
+	const indexOfFirstExercises = indexOfLastExercises - exercisesPerPage;
+	const currentExercises = exercises.slice(
+		indexOfFirstExercises,
+		indexOfLastExercises
+	);
+
+	const paginate = (e, value) => {
+		setCurrentPage(value);
+
+		window.scrollTo({ top: "1800px", behavior: "smooth" });
+	};
+
 	return (
 		<Box
 			id="exercises"
@@ -25,9 +42,24 @@ const Exercises = ({ setExercise, exercises, setBodyPart }) => {
 				flexWrap="wrap"
 				justifyContent="center"
 			>
-				{exercises.map((exercise, index) => (
-					<p key={index}>{exercise.name}</p>
+				{currentExercises.map((exercise, index) => (
+					<ExerciseCard key={index} exercise={exercise} />
 				))}
+			</Stack>
+
+			{/* Pagination */}
+
+			<Stack mt="100px" alignItems="center">
+				{exercises.length > 9 && (
+					<Pagination
+						color="standard"
+						defaultPage={1}
+						count={Math.ceil(exercises.length / exercisesPerPage)}
+						page={currentPage}
+						onChange={paginate}
+						size="large"
+					/>
+				)}
 			</Stack>
 		</Box>
 	);
